4ad7da6bee8bfb4ba806a8004ae4857dc80696a6
Jonatas Oliveira <jonatasoliveira1234@gmail.com>
2023-06-04 22:25:17 -0300

    Code fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { RouterProvider } from "react-router-dom";
 import router from "./router";
--- src/pages/MentorBoard/MentorBoard.tsx
+++ src/pages/MentorBoard/MentorBoard.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { useMentors } from "../../services/Mentors/useMentors";
 
 const MentorBoard = () => {
