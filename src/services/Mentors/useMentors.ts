6790533a09852a7c9a16afb84590a90bc9902a3d
anacpbarros <barros.anacp@gmail.com>
2023-07-19 19:50:07 -0300

    Create and find mentors endpoints

--- src/services/Mentors/useMentors.ts
+++ src/services/Mentors/useMentors.ts
@@ -1,10 +1,44 @@
 import { useQuery } from "react-query";
 import { api } from "../../services";
+import { User } from "../../Domains/User";
+import { Mentee } from "../../Domains/Mentee";
 
-const getMentors = () => {
-  return api.get("/mentor").then((res) => res.data);
+interface NewMentorProps {
+  user: User;
+  isAvailable: boolean;
+  stack: string[];
+  mentees?: Mentee[];
+}
+
+interface GetMentorsParams {
+  name?: string;
+  stack?: Array<string>;
+}
+
+const getMentors = (param?: GetMentorsParams) => {
+  if (!param) {
+    return api.get("/mentor").then((res) => res.data);
+
+  }
+  return api.get("/mentor", {params: param}).then((res) => res.data);
 };
 
-export const useMentors = () => {
-  return useQuery(["mentors"], getMentors);
+export const useMentors = (param?: GetMentorsParams) => {
+  const getMentorsQuery = async () => {
+    if (param) {
+      return await getMentors(param);
+    }
+    return await getMentors();
+  };
+
+  return useQuery(["mentors"], getMentorsQuery);
 };
+
+export const createNewMentor = (newMentorParams: NewMentorProps) => {
+  return api.post("/mentor", newMentorParams).then(() => console.log('Adicionou!!!'));
+};
+
+
+
+
+
