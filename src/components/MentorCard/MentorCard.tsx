f6fe3056b9aab91322ef5637b4c48e46b5acb8d0
Jonatas Oliveira <jonatasoliveira1234@gmail.com>
2023-07-30 16:39:05 -0300

    Improved Mentor Card and Panel Responsivity and updated Avatar Component

--- src/components/MentorCard/MentorCard.tsx
+++ src/components/MentorCard/MentorCard.tsx
@@ -1,4 +1,6 @@
 import {
+  Avatar,
+  AvatarBadge,
   Box,
   Card,
   CardBody,
@@ -7,11 +9,9 @@ import {
   Flex,
   Heading,
   Icon,
-  Image,
   Text,
   Tooltip,
 } from "@chakra-ui/react";
-import { FcOk } from "react-icons/fc";
 import { VscJson } from "react-icons/vsc";
 
 const MentorCard = () => {
@@ -19,32 +19,21 @@ const MentorCard = () => {
     <>
       <Card maxW="sm" cursor="pointer">
         <CardBody maxH="27vh">
-          <Flex justifyContent="space-between">
+          <Flex justifyContent="space-between" pr={[0, 7]}>
             <Tooltip label="Mentor Disponível">
               <Box position="relative" display="inline-block">
-                <Image
-                  src="https://images.unsplash.com/photo-1555041469-a586c61ea9bc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-                  alt="Green double couch with wooden legs"
-                  height={120}
-                  width="100%"
-                  borderRadius="full"
-                  boxSize={20}
-                />
-                <Icon
-                  as={FcOk}
-                  boxSize={7}
-                  marginLeft={2}
-                  position="absolute"
-                  right={1}
-                  bottom={1}
-                  color="white"
-                  borderRadius="full"
-                />
+                <Avatar name="Dan Abrahmov" src="https://bit.ly/dan-abramov">
+                  <AvatarBadge
+                    borderColor="papayawhip"
+                    bg="green.500"
+                    boxSize="1em"
+                  />
+                </Avatar>
               </Box>
             </Tooltip>
-            <Box mt={4}>
+            <Box mt={[2, 0]} ml={[0, 6]}>
               <Heading size="md">John Doe</Heading>
-              <Text>Analista de desenvolvimento Sr</Text>
+              <Text>Analista de Desenvolvimento Sr</Text>
             </Box>
           </Flex>
         </CardBody>
--- src/components/MentorPanel/MentorPanel.tsx
+++ src/components/MentorPanel/MentorPanel.tsx
@@ -4,51 +4,144 @@ import MentorCard from "../MentorCard/MentorCard";
 const MentorPanel = () => {
   return (
     <Stack
-      paddingLeft={20}
-      paddingRight={20}
+      paddingLeft={[10, 20]}
+      paddingRight={[10, 18]}
       spacing={4}
       marginTop={4}
       overflow="auto"
       maxHeight="80vh"
     >
-      <Flex flexWrap="wrap" gap={2}>
-        <Box p={4} width="calc(25% - 10px)">
+      <Flex
+        flexWrap="wrap"
+        justifyContent={["center", "flex-start", "flex-start", "flex-start"]}
+      >
+        <Box
+          p={4}
+          width="auto"
+          flexBasis={[
+            "100%", // Mobile: 1 card per line
+            "calc(50% - 20px)", // 2 cards in a line
+            "calc(33.33% - 20px)", // 3 cards in a line
+            "calc(25% - 20px)", // 4 cards in a line
+          ]}
+        >
           <MentorCard />
         </Box>
 
-        <Box p={4} width="calc(25% - 10px)">
+        <Box
+          p={4}
+          width="auto"
+          flexBasis={[
+            "100%", // Mobile: 1 card per line
+            "calc(50% - 20px)", // 2 cards in a line
+            "calc(33.33% - 20px)", // 3 cards in a line
+            "calc(25% - 20px)", // 4 cards in a line
+          ]}
+        >
           <MentorCard />
         </Box>
 
-        <Box p={4} width="calc(25% - 10px)">
+        <Box
+          p={4}
+          width="auto"
+          flexBasis={[
+            "100%", // Mobile: 1 card per line
+            "calc(50% - 20px)", // 2 cards in a line
+            "calc(33.33% - 20px)", // 3 cards in a line
+            "calc(25% - 20px)", // 4 cards in a line
+          ]}
+        >
           <MentorCard />
         </Box>
 
-        <Box p={4} width="calc(25% - 10px)">
+        <Box
+          p={4}
+          width="auto"
+          flexBasis={[
+            "100%", // Mobile: 1 card per line
+            "calc(50% - 20px)", // 2 cards in a line
+            "calc(33.33% - 20px)", // 3 cards in a line
+            "calc(25% - 20px)", // 4 cards in a line
+          ]}
+        >
           <MentorCard />
         </Box>
 
-        <Box p={4} width="calc(25% - 10px)">
+        <Box
+          p={4}
+          width="auto"
+          flexBasis={[
+            "100%", // Mobile: 1 card per line
+            "calc(50% - 20px)", // 2 cards in a line
+            "calc(33.33% - 20px)", // 3 cards in a line
+            "calc(25% - 20px)", // 4 cards in a line
+          ]}
+        >
           <MentorCard />
         </Box>
 
-        <Box p={4} width="calc(25% - 10px)">
+        <Box
+          p={4}
+          width="auto"
+          flexBasis={[
+            "100%", // Mobile: 1 card per line
+            "calc(50% - 20px)", // 2 cards in a line
+            "calc(33.33% - 20px)", // 3 cards in a line
+            "calc(25% - 20px)", // 4 cards in a line
+          ]}
+        >
           <MentorCard />
         </Box>
 
-        <Box p={4} width="calc(25% - 10px)">
+        <Box
+          p={4}
+          width="auto"
+          flexBasis={[
+            "100%", // Mobile: 1 card per line
+            "calc(50% - 20px)", // 2 cards in a line
+            "calc(33.33% - 20px)", // 3 cards in a line
+            "calc(25% - 20px)", // 4 cards in a line
+          ]}
+        >
           <MentorCard />
         </Box>
 
-        <Box p={4} width="calc(25% - 10px)">
+        <Box
+          p={4}
+          width="auto"
+          flexBasis={[
+            "100%", // Mobile: 1 card per line
+            "calc(50% - 20px)", // 2 cards in a line
+            "calc(33.33% - 20px)", // 3 cards in a line
+            "calc(25% - 20px)", // 4 cards in a line
+          ]}
+        >
           <MentorCard />
         </Box>
 
-        <Box p={4} width="calc(25% - 10px)">
+        <Box
+          p={4}
+          width="auto"
+          flexBasis={[
+            "100%", // Mobile: 1 card per line
+            "calc(50% - 20px)", // 2 cards in a line
+            "calc(33.33% - 20px)", // 3 cards in a line
+            "calc(25% - 20px)", // 4 cards in a line
+          ]}
+        >
           <MentorCard />
         </Box>
 
-        <Box p={4} width="calc(25% - 10px)">
+        <Box
+          p={4}
+          width="auto"
+          flexBasis={[
+            "100%", // Mobile: 1 card per line
+            "calc(50% - 20px)", // 2 cards in a line
+            "calc(33.33% - 20px)", // 3 cards in a line
+            "calc(25% - 20px)", // 4 cards in a line
+          ]}
+        >
           <MentorCard />
         </Box>
       </Flex>
